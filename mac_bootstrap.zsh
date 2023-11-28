ea65fe799a2623e82f64c3749e1437514e33fede
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-11-28 18:31:27 -0500

    improved install apps section by making a function that covers different scenarios

--- mac_bootstrap.zsh
+++ mac_bootstrap.zsh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+# Script for bootstrapping a new MacBook for development.
+# This script installs necessary tools and sets up symlinks for dotfiles.
+# Assumptions: macOS with internet access and standard file system structure.
+# Usage: Execute this script in a zsh shell.
+
 # Define ANSI color escape codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -8,6 +13,8 @@ YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No color (reset)
+
+# BEGINNING OF FUNCTIONS ------------------------------------------------------
 
 # Function to display colored messages
 color_echo() {
@@ -33,7 +40,7 @@ centered_color_echo() {
     echo -e "${color}${padding}${message}${padding}${NC}"
 }
 
-# Attempt to clone using SSH, fallback to HTTPS if SSH fails
+# Function to attempt to clone using SSH, fallback to HTTPS if SSH fails
 git_clone_fallback() {
     local ssh_url="$1"
     local https_url="$2"
@@ -41,103 +48,7 @@ git_clone_fallback() {
     git clone "$ssh_url" "$clone_directory" || git clone "$https_url" "$clone_directory" || { color_echo $RED "Failed to clone repository."; exit 1; }
 }
 
-# Script for bootstrapping a new MacBook for development.
-# This script installs necessary tools and sets up symlinks for dotfiles.
-# Assumptions: macOS with internet access and standard file system structure.
-# Usage: Execute this script in a zsh shell.
-
-# Step 1: Install Xcode Command Line Tools
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- Xcode Command Line Tools Configuration -------------->"
-
-echo "" # Print a blank line
-
-if ! xcode-select -p &>/dev/null; then
-    color_echo $RED "Installing Xcode Command Line Tools..."
-    xcode-select --install || { color_echo $RED "Failed to install Xcode Command Line Tools."; exit 1; }
-
-    # Wait for Xcode Command Line Tools installation to complete
-    until xcode-select -p &>/dev/null; do
-        color_echo $YELLOW "Waiting for Xcode Command Line Tools to complete installation..."
-        sleep 30
-    done
-
-    color_echo $GREEN "Xcode Command Line Tools installation complete."
-else
-    color_echo $GREEN "Xcode Command Line Tools already installed."
-fi
-
-# Step 2: Install Homebrew
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- Homebrew Configuration -------------->"
-
-echo "" # Print a blank line
-
-if ! command -v brew &>/dev/null; then
-    color_echo $BLUE "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { color_echo $RED "Failed to install Homebrew."; exit 1; }
-else
-    color_echo $GREEN "Homebrew already installed."
-fi
-
-# Step 3: Install Git (if not already installed by Xcode Command Line Tools)
-if ! command -v git &>/dev/null; then
-    color_echo $BLUE "Installing Git..."
-    brew install git || { color_echo $RED "Failed to install Git."; exit 1; }
-fi
-
-# Step 4: Clone .dotfiles repository
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- Dotfiles + BootStrap Repository Configuration -------------->"
-
-echo "" # Print a blank line
-
-DOTFILES_DIR="$HOME/.dotfiles"
-if [ ! -d "$DOTFILES_DIR" ]; then
-    color_echo $BLUE "Cloning .dotfiles repository..."
-    git clone "https://github.com/av1155/.dotfiles.git" "$DOTFILES_DIR" || \
-        { color_echo $RED "Failed to clone .dotfiles repository."; exit 1; }
-else
-    color_echo $GREEN "The '.dotfiles' directory already exists. Skipping clone of repository."
-fi
-
-# Step 5: Install software from Brewfile
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- Brewfile Configuration -------------->"
-
-echo "" # Print a blank line
-
-color_echo $BLUE "Installing software from Brewfile..."
-brew bundle --file "$DOTFILES_DIR/Brewfile" || { color_echo $RED "Failed to install software from Brewfile."; exit 1; }
-
-# Validate TMUX_CONFIG_DIR
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- tmux Config Directory Setup -------------->"
-
-echo "" # Print a blank line
-
-TMUX_CONFIG_DIR="$HOME/.config/tmux"
-if [ ! -d "$TMUX_CONFIG_DIR" ]; then
-    color_echo $BLUE "Creating tmux config directory..."
-    mkdir -p "$TMUX_CONFIG_DIR"
-else
-    color_echo $GREEN "The tmux config directory already exists. Skipping configuration."
-fi
-
-# Step 6: Create symlinks (Idempotent) ----------------------------------------
-echo "" # Print a blank line
-
-centered_color_echo $ORANGE "<-------------- Symlinks Configuration -------------->"
-
-echo "" # Print a blank line
-
-color_echo $BLUE "Creating symlinks..."
-
+# Function to create a symlink
 create_symlink() {
     local source_file="$1"
     local target_file="$2"
@@ -170,84 +81,176 @@ create_symlink() {
     ln -sf "$source_file" "$target_file" || { color_echo $RED "Failed to create symlink for $(basename "$source_file")"; exit 1; }
 }
 
-# Symlinks go here:
+# Function to prompt and install an app
+install_app() {
+    local app_name="$1"
+    local install_command="$2"
+    local check_command="$3"
 
+    if eval "$check_command"; then
+        color_echo $GREEN "$app_name already installed."
+    else
+        color_echo $YELLOW "Do you want to install $app_name? (y/n)"
+        echo -n "Enter choice: > "
+        read -r choice
+        if [ "$choice" = "y" ]; then
+            color_echo $BLUE "Installing $app_name..."
+            eval "$install_command" || { color_echo $RED "Failed to install $app_name."; exit 1; }
+        else
+            color_echo $BLUE "Skipping $app_name installation."
+        fi
+    fi
+}
+
+# END OF FUNCTIONS ------------------------------------------------------------
+
+# Step 1: Install Xcode Command Line Tools -------------------------------------
+echo ""
+
+centered_color_echo $ORANGE "<-------------- Xcode Command Line Tools Configuration -------------->"
+
+echo ""
+
+if ! xcode-select -p &>/dev/null; then
+    color_echo $RED "Installing Xcode Command Line Tools..."
+    xcode-select --install || { color_echo $RED "Failed to install Xcode Command Line Tools."; exit 1; }
+
+    # Wait for Xcode Command Line Tools installation to complete
+    until xcode-select -p &>/dev/null; do
+        color_echo $YELLOW "Waiting for Xcode Command Line Tools to complete installation..."
+        sleep 30
+    done
+
+    color_echo $GREEN "Xcode Command Line Tools installation complete."
+else
+    color_echo $GREEN "Xcode Command Line Tools already installed."
+fi
+
+# Step 2: Install Homebrew ----------------------------------------------------
+echo ""
+
+centered_color_echo $ORANGE "<-------------- Homebrew Configuration -------------->"
+
+echo ""
+
+if ! command -v brew &>/dev/null; then
+    color_echo $BLUE "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { color_echo $RED "Failed to install Homebrew."; exit 1; }
+else
+    color_echo $GREEN "Homebrew already installed."
+fi
+
+# Step 3: Install Git (if not already installed by Xcode Command Line Tools) ---
+if ! command -v git &>/dev/null; then
+    color_echo $BLUE "Installing Git..."
+    brew install git || { color_echo $RED "Failed to install Git."; exit 1; }
+fi
+
+# Step 4: Clone .dotfiles repository -------------------------------------------
+echo ""
+
+centered_color_echo $ORANGE "<-------------- Dotfiles + BootStrap Repository Configuration -------------->"
+
+echo ""
+
+DOTFILES_DIR="$HOME/.dotfiles"
+if [ ! -d "$DOTFILES_DIR" ]; then
+    color_echo $BLUE "Cloning .dotfiles repository..."
+    git clone "https://github.com/av1155/.dotfiles.git" "$DOTFILES_DIR" || \
+        { color_echo $RED "Failed to clone .dotfiles repository."; exit 1; }
+else
+    color_echo $GREEN "The '.dotfiles' directory already exists. Skipping clone of repository."
+fi
+
+# Step 5: Install software from Brewfile ---------------------------------------
+echo ""
+
+centered_color_echo $ORANGE "<-------------- Brewfile Configuration -------------->"
+
+echo ""
+
+color_echo $BLUE "Installing software from Brewfile..."
+brew bundle --file "$DOTFILES_DIR/Brewfile" || { color_echo $RED "Failed to install software from Brewfile."; exit 1; }
+
+# Validate TMUX_CONFIG_DIR ----------------------------------------------------
+echo ""
+
+centered_color_echo $ORANGE "<-------------- tmux Config Directory Setup -------------->"
+
+echo ""
+
+TMUX_CONFIG_DIR="$HOME/.config/tmux"
+if [ ! -d "$TMUX_CONFIG_DIR" ]; then
+    color_echo $BLUE "Creating tmux config directory..."
+    mkdir -p "$TMUX_CONFIG_DIR"
+else
+    color_echo $GREEN "The tmux config directory already exists. Skipping configuration."
+fi
+
+# Step 6: Create symlinks (Idempotent) ----------------------------------------
+
+echo ""
+
+centered_color_echo $ORANGE "<-------------- Symlinks Configuration -------------->"
+
+echo ""
+
+color_echo $BLUE "Creating symlinks..."
+
+# Symlinks go here:
 # create_symlink "$DOTFILES_DIR/configs/.original_file" "$HOME/.linked_file"
 create_symlink "$DOTFILES_DIR/configs/.zshrc" "$HOME/.zshrc"
 create_symlink "$DOTFILES_DIR/configs/.gitconfig" "$HOME/.gitconfig"
 create_symlink "$DOTFILES_DIR/configs/tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
-# Installation of of software -----------------------------------------------
-echo "" # Print a blank line
+# Installation of software -----------------------------------------------
+
+echo ""
 
 centered_color_echo $ORANGE "<-------------- Installation of Software -------------->"
 
-echo "" # Print a blank line
+echo ""
+
+# app_name: The name of the app.
+# install_command: The command to install the app.
+# check_command: The command to check if the app is already installed.
+
+# Example:
+# install_app "Visual Studio Code" "brew install --cask visual-studio-code" "! brew list --cask | grep -q visual-studio-code && [ ! -d '/Applications/Visual Studio Code.app' ]"
 
 # Install iTerm2
-if ! brew list --cask | grep -q iterm2 && [ ! -d "/Applications/iTerm.app" ]; then
-    color_echo $BLUE "Installing iTerm2..."
-    brew install --cask iterm2 || { color_echo $RED "Failed to install iTerm2."; exit 1; }
-else
-    color_echo $GREEN "iTerm2 already installed."
-fi
+install_app "iTerm2" "brew install --cask iterm2" "! brew list --cask | grep -q iterm2 && [ ! -d '/Applications/iTerm.app' ]"
 
 # Install Docker
-if ! brew list --cask | grep -q docker && [ ! -d "/Applications/Docker.app" ]; then
-    color_echo $BLUE "Installing Docker..."
-    brew install --cask docker || { color_echo $RED "Failed to install Docker."; exit 1; }
-else
-    color_echo $GREEN "Docker already installed."
-fi
+install_app "Docker" "brew install --cask docker" "! brew list --cask | grep -q docker && [ ! -d '/Applications/Docker.app' ]"
 
 # Install Miniforge3
-if [ ! -d "$HOME/miniforge3" ]; then
-    color_echo $BLUE "Installing Miniforge3..."
-    brew install miniforge || { color_echo $RED "Failed to install Miniforge3."; exit 1; }
-else
-    color_echo $GREEN "Miniforge3 already installed."
-fi
+install_app "Miniforge3" "brew install miniforge" "[ ! -d '$HOME/miniforge3' ]"
 
 # Install Google Chrome
-if ! brew list --cask | grep -q google-chrome && [ ! -d "/Applications/Google Chrome.app" ]; then
-    color_echo $BLUE "Installing Google Chrome..."
-    brew install --cask google-chrome || { color_echo $RED "Failed to install Google Chrome."; exit 1; }
-else
-    color_echo $GREEN "Google Chrome already installed."
-fi
+install_app "Google Chrome" "brew install --cask google-chrome" "! brew list --cask | grep -q google-chrome && [ ! -d '/Applications/Google Chrome.app' ]"
 
 # Install Visual Studio Code
-if ! brew list --cask | grep -q visual-studio-code && [ ! -d "/Applications/Visual Studio Code.app" ]; then
-    color_echo $BLUE "Installing Visual Studio Code..."
-    brew install --cask visual-studio-code || { color_echo $RED "Failed to install Visual Studio Code."; exit 1; }
-else
-    color_echo $GREEN "Visual Studio Code already installed."
-fi
+install_app "Visual Studio Code" "brew install --cask visual-studio-code" "! brew list --cask | grep -q visual-studio-code && [ ! -d '/Applications/Visual Studio Code.app' ]"
 
 # Install JetBrains Toolbox
-if ! brew list --cask | grep -q jetbrains-toolbox && [ ! -d "/Applications/JetBrains Toolbox.app" ]; then
-    color_echo $BLUE "Installing JetBrains Toolbox..."
-    brew install --cask jetbrains-toolbox || { color_echo $RED "Failed to install JetBrains Toolbox."; exit 1; }
-else
-    color_echo $GREEN "JetBrains Toolbox already installed."
-fi
+install_app "JetBrains Toolbox" "brew install --cask jetbrains-toolbox" "! brew list --cask | grep -q jetbrains-toolbox && [ ! -d '/Applications/JetBrains Toolbox.app' ]"
 
 # Install Ollama
-if ! brew list | grep -q ollama && [ ! -d "/Applications/Ollama.app" ]; then
-    color_echo $BLUE "Installing Ollama..."
-    brew install ollama || { color_echo $RED "Failed to install Ollama."; exit 1; }
-else
-    color_echo $GREEN "Ollama already installed."
-fi
+install_app "Ollama" "brew install ollama" "! brew list | grep -q ollama && [ ! -d '/Applications/Ollama.app' ]"
+
+# Install Oh My Zsh
+install_app "Oh My Zsh" "sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"" "[ ! -d '$HOME/.oh-my-zsh' ]"
 
 # -----------------------------------------------------------------------------
-echo "" # Print a blank line
+
+echo ""
 
 centered_color_echo $ORANGE "<-------------- Configuration of NVM, NODE, & NPM -------------->"
 
-echo "" # Print a blank line
+echo ""
 
-# Check if NVM (Node Version Manager) is installed
+# Check if NVM (Node Version Manager) is installed ----------------------------
 if ! command -v nvm &>/dev/null; then
     # Install NVM if it's not installed
     color_echo $BLUE "Installing Node Version Manager (nvm)..."
@@ -258,9 +261,9 @@ else
     color_echo $GREEN "NVM already installed."
 fi
 
-echo "" # Print a blank line
+echo ""
 
-# Check if Node is installed
+# Check if Node is installed --------------------------------------------------
 if ! command -v node &>/dev/null; then
     # Install Node.js using NVM if it's not installed
     color_echo $BLUE "Installing Node.js..."
@@ -269,9 +272,9 @@ else
     color_echo $GREEN "Node.js already installed."
 fi
 
-echo "" # Print a blank line
+echo ""
 
-# Install Global npm Packages:
+# Install Global npm Packages: ------------------------------------------------
 color_echo $BLUE "Installing global npm packages..."
 
 color_echo $BLUE "tree-sitter-cli: "
@@ -281,13 +284,14 @@ color_echo $BLUE "live-server: "
 npm install -g live-server || { color_echo $RED "Failed to install live-server."; exit 1; }
 
 # -----------------------------------------------------------------------------
-echo "" # Print a blank line
+
+# Install JetBrainsMono Nerd Font ---------------------------------------------
+echo ""
 
 centered_color_echo $ORANGE "<-------------- Configuration of Nerd Fonts -------------->"
 
-echo "" # Print a blank line
+echo ""
 
-# Install JetBrainsMono Nerd Font
 color_echo $BLUE "Installing JetBrainsMono Nerd Font..."
 FONT_DIR="$HOME/Library/Fonts"
 if [ ! -d "$FONT_DIR" ]; then
@@ -300,11 +304,12 @@ rm "$FONT_DIR/JetBrainsMono.zip"
 color_echo $GREEN "JetBrainsMono Nerd Font installation complete."
 
 # AstroNvim Installation ------------------------------------------------------
-echo "" # Print a blank line
+
+echo ""
 
 centered_color_echo $ORANGE "<-------------- AstroNvim Configuration -------------->"
 
-echo "" # Print a blank line
+echo ""
 
 # Check if ~/.config/nvim exists
 if [ -d "$HOME/.config/nvim" ]; then
@@ -335,7 +340,7 @@ else
     git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 fi
 
-echo "" # Print a blank line
+echo ""
 
 # Check if ~/.config/nvim/lua/user exists
 if [ -d "$HOME/.config/nvim/lua/user" ]; then
@@ -362,11 +367,11 @@ else
 fi
 
 # -----------------------------------------------------------------------------
-echo "" # Print a blank line
+echo ""
 
 centered_color_echo $ORANGE "<-------------- TODO List of Apps to Download -------------->"
 
-echo "" # Print a blank line
+echo ""
 
 # Define the list of apps
 app_list=(
@@ -429,6 +434,8 @@ printf "%s\n" "${app_list[@]}" > "$desktop_path"
 
 # Print a message to inform the user
 color_echo $BLUE "A TODO list of apps to download has been created on your desktop: $desktop_path"
+
+# -----------------------------------------------------------------------------
 
 echo "" # Print a blank line
 
